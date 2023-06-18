a85a1183c0efe4582fd9bb68755cb460c88f4357
victorbjorsvik <126663593+victorbjorsvik@users.noreply.github.com>
2023-06-18 22:59:09 +0200

    endret skog

---
+++ app_lite.py
@@ -0,0 +1,55 @@
+import os
+import csv
+
+from flask import Flask, flash, redirect, render_template, request, session
+from flask_session import Session
+from tempfile import mkdtemp
+from werkzeug.security import check_password_hash, generate_password_hash
+
+from helpers import apology, login_required
+
+# Configure application
+app = Flask(__name__)
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+
+@app.after_request
+def after_request(response):
+    """Ensure responses aren't cached"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+
+@app.route("/")
+def index():
+    """Show portfolio of stocks"""
+
+
+    return render_template("index.html")
+
+
+
+@app.route("/kontakt")
+def kontakt():
+
+
+    return render_template("kontakt.html")
+
+
+@app.route("/redegjorelse")
+def redegjorelse():
+    return render_template("redegjorelse.html")
+
+
+
+
+
+
+
+
